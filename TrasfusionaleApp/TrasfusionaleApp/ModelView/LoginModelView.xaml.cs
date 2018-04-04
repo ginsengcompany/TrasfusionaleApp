262ae44cfc48d3f721dff49fda052bc7fcf82d71
FabriGinseng <antoniofabriziofiume@libero.it>
2018-04-04 15:17:26 +0200

    aggiunti i plugin per il qrcode e aggiunte le pagine di login

---
+++ TrasfusionaleApp/TrasfusionaleApp/ModelView/LoginModelView.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrasfusionaleApp.ModelView
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class LoginModelView : ContentPage
+	{
+		public LoginModelView ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
