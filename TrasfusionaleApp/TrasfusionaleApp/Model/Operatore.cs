1ce8a95e6f520db24a0adebc4d299f23c6975e53
FabriGinseng <antoniofabriziofiume@libero.it>
2018-04-09 12:36:18 +0200

    classe operatore

---
+++ TrasfusionaleApp/TrasfusionaleApp/Model/Operatore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrasfusionaleApp.Model
+{
+    public class Operatore
+    {
+        public string uid { get; set; }
+        public string nome { get; set; }
+        public string cognome { get; set; }
+        public string tipoOperatore { get; set; }
+        public int codice_operatore { get; set; }
+    }
+}
