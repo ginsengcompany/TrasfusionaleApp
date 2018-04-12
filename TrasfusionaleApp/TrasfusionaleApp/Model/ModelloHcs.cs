d6e21be723dfd6da9b2dd0661178c7fe898b138d
aldominasi <aldo.minasi@gmail.com>
2018-04-12 10:05:46 +0200

    ho inserito la classe sacca

--- TrasfusionaleApp/TrasfusionaleApp/Model/ModelloHcs.cs
+++ TrasfusionaleApp/TrasfusionaleApp/Model/ModelloHcs.cs
@@ -6,5 +6,10 @@ namespace TrasfusionaleApp.Model
 {
     public class ModelloHcs
     {
+        public bool compatibilitaImmunologica { get; set; }
+        public bool ispezioneEmocomponenti { get; set; }
+        public bool identificazioneRiceventeRichiesta { get; set; }
+        public bool identificazioneRiceventeScan { get; set; }
+        public bool verificaOgniUnitaDaTrasfondere { get; set; }
     }
 }
