7e53ac3185c17a719e4b76d7a559b77b8bafa26d
FabriGinseng <antoniofabriziofiume@libero.it>
2018-04-13 15:23:21 +0200

    implementata connessione per controllo sacca paziente

--- TrasfusionaleApp/TrasfusionaleApp/Model/Sacca.cs
+++ TrasfusionaleApp/TrasfusionaleApp/Model/Sacca.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using TrasfusionaleApp.Service;
 
 namespace TrasfusionaleApp.Model
 {
@@ -8,5 +10,25 @@ namespace TrasfusionaleApp.Model
     {
         public string uid { get; set; }
         public string uidPaziente { get; set; }
+        private readonly string datiSacca = "http://192.168.125.14:3000/sacche/datisacca";
+
+        public async Task<bool> InviaSacca()
+        {
+            REST<Sacca, Sacca> connessioneDatiSacca = new REST<Sacca, Sacca>();
+            List<Header> headers = new List<Header>();
+            headers.Add(new Header("access-token", App.Current.Properties["access-token"].ToString()));
+            var result = await connessioneDatiSacca.PostJson(datiSacca, this, headers);
+            if (connessioneDatiSacca.responseMessage != System.Net.HttpStatusCode.OK)
+            {
+                await App.Current.MainPage.DisplayAlert("Attenzione", connessioneDatiSacca.warning, "OK");
+                return false;
+            }
+            else
+            {
+                this.uidPaziente = result.uidPaziente;
+                return true;
+            }
+        }
     }
+
 }
--- TrasfusionaleApp/TrasfusionaleApp/Views/PrimoPassaggioTrasfusione.xaml.cs
+++ TrasfusionaleApp/TrasfusionaleApp/Views/PrimoPassaggioTrasfusione.xaml.cs
@@ -3,18 +3,137 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using TrasfusionaleApp.Model;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ZXing.Net.Mobile.Forms;
 
 namespace TrasfusionaleApp.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class PrimoPassaggioTrasfusione : ContentPage
 	{
-		public PrimoPassaggioTrasfusione ()
+	    private Sacca sacca;
+	    private bool scanSaccaEseguita = false, scanPazienteEseguita=false;
+	    private Paziente paziente;
+	    private bool pazienteScan = false, saccaScan = false;
+
+        public PrimoPassaggioTrasfusione ()
 		{
 			InitializeComponent ();
 		}
-	}
+
+	    private async void ScannerizzaSacca(object sender, EventArgs e)
+	    {
+	        var scanPage = new ZXingScannerPage();
+	        scanPage.OnScanResult += (result) => {
+	            // Stop scanning
+	            scanPage.IsScanning = false;
+	            // Pop the page and show the result
+	            Device.BeginInvokeOnMainThread(() => {
+	                Navigation.PopAsync();
+	                sacca = new Sacca();
+	                sacca.uid = result.Text;
+	                scanSaccaEseguita = true;
+	                controllaScanSacca();
+	            });
+	        };
+	        // Navigate to our scanner page
+	        await Navigation.PushAsync(scanPage);
+        }
+
+	    private async void ScannerizzaPaziente(object sender, EventArgs e)
+	    {
+	        var scanPage = new ZXingScannerPage();
+	        scanPage.OnScanResult += (result) => {
+	            // Stop scanning
+	            scanPage.IsScanning = false;
+
+	            // Pop the page and show the result
+	            Device.BeginInvokeOnMainThread(() => {
+	                Navigation.PopAsync();
+	                paziente = new Paziente();
+	                paziente.uid = result.Text;
+	                scanPazienteEseguita = true;
+	                controllaScanPaziente();
+	            });
+	        };
+	        // Navigate to our scanner page
+	        await Navigation.PushAsync(scanPage);
+
+        }
+        private async void controllaScanSacca()
+        {
+            if (scanSaccaEseguita)
+            {
+                scanSaccaEseguita = false;
+                if (await sacca.InviaSacca())
+                {
+                    labelSacca.Text = sacca.uid;
+                    saccaScan = true;
+                    if (pazienteScan && saccaScan)
+                    {
+                        if (sacca.uidPaziente == paziente.uid)
+                        {
+                            await DisplayAlert("Pre-Trasfusionale", "Associazione corretta", "OK");
+                            btnPrelievo.IsEnabled = true;
+                        }
+                        else
+                        {
+                            await DisplayAlert("Pre-Trasfusionale", "Associazione non corretta", "OK");
+                           btnPrelievo.IsEnabled = false;
+                        }
+                    }
+                   else
+                        btnPrelievo.IsEnabled = false;
+                }
+                else
+                {
+                    labelSacca.Text = "";
+                    saccaScan = false;
+                    btnPrelievo.IsEnabled = pazienteScan && saccaScan;
+                }
+            }
+        }
+
+        private async void controllaScanPaziente()
+        {
+            if (scanPazienteEseguita)
+            {
+                scanPazienteEseguita = false;
+                if (await paziente.prelevaDatiPaziente())
+                {
+                    NomePaziente.Text = paziente.nome;
+                    CognomePaziente.Text = paziente.cognome;
+                    RepartoPaziente.Text = paziente.reparto;
+                    LettoPaziente.Text = paziente.letto.ToString();
+                    pazienteScan = true;
+                    if (pazienteScan && saccaScan)
+                    {
+                        if (sacca.uidPaziente == paziente.uid)
+                        {
+                            await DisplayAlert("Pre-Trasfusionale", "Associazione corretta", "OK");
+                           btnPrelievo.IsEnabled = true;
+                        }
+                        else
+                        {
+                            await DisplayAlert("Pre-Trasfusionale", "Associazione non corretta", "OK");
+                            btnPrelievo.IsEnabled = false;
+                        }
+                    }
+                    else
+                        btnPrelievo.IsEnabled = false;
+                }
+                else
+                {
+                    NomePaziente.Text = "";
+                    CognomePaziente.Text = "";
+                    RepartoPaziente.Text = "";
+                    LettoPaziente.Text = "";
+                    pazienteScan = false;
+                  btnPrelievo.IsEnabled = pazienteScan && saccaScan;
+                }
+            }
+        }
+    }
 }
