017d82b4c39844335c1885700d92ad746288cd3b
aldominasi <aldo.minasi@gmail.com>
2018-04-13 10:51:43 +0200

    ho inserito una classe privata nella pagina CompilazioneModelloH per poter inviare gli operatori e i modelli compilati al servizio dedicato al confronto tra questi

--- TrasfusionaleApp/TrasfusionaleApp/Views/CompilazioneModelloH.xaml.cs
+++ TrasfusionaleApp/TrasfusionaleApp/Views/CompilazioneModelloH.xaml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrasfusionaleApp.Model;
+using TrasfusionaleApp.Service;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -30,29 +31,72 @@ namespace TrasfusionaleApp.Views
 
 	    }
 
-        private void InvioDati(object sender, EventArgs e)
+        private async void InvioDati(object sender, EventArgs e)
 	    {
-	        if (entryConfermaPassword.Text == infermiere.password)
+	        if (medico == default(Operatore))
 	        {
-	            if (infermiere.codice_operatore != 1)
+	            if (entryConfermaPassword.Text == infermiere.password)
 	            {
 	                modelloInf.compatibilitaImmunologica = compatibilitaImmunologica.IsToggled;
 	                modelloInf.identificazioneRiceventeRichiesta = identificazioneRiceventeRichiesta.IsToggled;
 	                modelloInf.identificazioneRiceventeScan = identificazioneRiceventeScan.IsToggled;
 	                modelloInf.ispezioneEmocomponenti = ispezioneEmocomponenti.IsToggled;
 	                modelloInf.verificaOgniUnitaDaTrasfondere = verificaOgniUnitaDaTrasfondere.IsToggled;
-
-                    Navigation.PushAsync(new LoginTrasfusionale(infermiere,modelloInf));
+                    await Navigation.PushAsync(new LoginTrasfusionale(infermiere,modelloInf));
                 }
                 else
-	            {
-
-	            }
+                    await DisplayAlert("Attenzione", "password non corretta", "ok");
             }
             else
 	        {
-	            DisplayAlert("Attenzione", "password non corretta", "ok");
-	        }
+                if (entryConfermaPassword.Text == medico.password)
+                {
+                    modelloMed.compatibilitaImmunologica = compatibilitaImmunologica.IsToggled;
+                    modelloMed.identificazioneRiceventeRichiesta = identificazioneRiceventeRichiesta.IsToggled;
+                    modelloMed.identificazioneRiceventeScan = identificazioneRiceventeScan.IsToggled;
+                    modelloMed.ispezioneEmocomponenti = ispezioneEmocomponenti.IsToggled;
+                    modelloMed.verificaOgniUnitaDaTrasfondere = verificaOgniUnitaDaTrasfondere.IsToggled;
+                    InvioModelliCompilati modelliCompilati = new InvioModelliCompilati(modelloInf, modelloMed, infermiere, medico);
+                    var result = await modelliCompilati.inviaDati();
+                    if (result)
+                        await Navigation.PushAsync(new PrimoPassaggioTrasfusione());
+                    else
+                    {
+                        await DisplayAlert("Attenzione", "I modelli compilati non risultano correttamente idonei per proseguire nel processo trasfusionale", "OK");
+                        await Navigation.PushModalAsync(new MainPage(infermiere));
+                    }
+                }
+                else
+                    await DisplayAlert("Attenzione", "password non corretta", "ok");
+            }
 	    }
+
+        private class InvioModelliCompilati
+        {
+            public ModelloHcs modello_medico { get; set; }
+            public ModelloHcs modello_infermiere { get; set; }
+            public Operatore infermiere { get; set; }
+            public Operatore medico { get; set; }
+            private readonly string restInvioModelliCompilati = "";
+
+            public InvioModelliCompilati() { }
+            public InvioModelliCompilati(ModelloHcs modelloInf, ModelloHcs modelloMed, Operatore infermiere, Operatore medico)
+            {
+                this.modello_medico = modelloMed;
+                this.modello_infermiere = modelloInf;
+                this.medico = medico;
+                this.infermiere = infermiere;
+            }
+
+            public async Task<bool> inviaDati()
+            {
+                REST<InvioModelliCompilati, bool> connessioneModelliCompilati = new REST<InvioModelliCompilati, bool>();
+                var result = await connessioneModelliCompilati.PostJson(restInvioModelliCompilati, this);
+                if (connessioneModelliCompilati.responseMessage != System.Net.HttpStatusCode.OK)
+                    return false;
+                else
+                    return true;
+            }
+        }
 	}
 }
