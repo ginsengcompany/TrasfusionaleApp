c2f39b94f3cd2d822ab85e03dbd0199765ee7422
d4rk413x21 <darkalex21@gmail.com>
2018-04-19 12:39:20 +0200

    Modificato layout mainpage e entry nel modello h

--- TrasfusionaleApp/TrasfusionaleApp.Android/MainActivity.cs
+++ TrasfusionaleApp/TrasfusionaleApp.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Xfx;
 
 namespace TrasfusionaleApp.Droid
 {
@@ -18,6 +19,7 @@ namespace TrasfusionaleApp.Droid
             ToolbarResource = Resource.Layout.Toolbar;
 
             base.OnCreate(bundle);
+            XfxControls.Init();
             global::Xamarin.Forms.Forms.Init(this, bundle);
             ZXing.Net.Mobile.Forms.Android.Platform.Init();
 
